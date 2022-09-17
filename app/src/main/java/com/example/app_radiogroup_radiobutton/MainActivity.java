05bc35a4294bf6560a33a7b8e2580d742a4b786d
AndresCar13 <andres2204120@correo.uis.edu.co>
2022-09-16 19:44:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/app_radiogroup_radiobutton/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.app_radiogroup_radiobutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et1, et2;
+    private TextView tv1;
+    private RadioButton rb1, rb2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = findViewById(R.id.et_val1);
+        et2 = findViewById(R.id.et_val2);
+        tv1 = findViewById(R.id.tv_res);
+        rb1 = findViewById(R.id.rb_sumar);
+        rb2 = findViewById(R.id.rb_restar);
+    }
+
+    public void Calcular (View view) {
+        String resultado;
+        int res;
+        String val1_String = et1.getText().toString();
+        String val2_String = et2.getText().toString();
+
+        int valor1_int = Integer.parseInt(val1_String);
+        int valor2_int = Integer.parseInt(val2_String);
+
+        if (rb1.isChecked()) {
+            res = valor1_int + valor2_int;
+        } else if (rb2.isChecked()) {
+            res = valor1_int - valor2_int;
+        } else {
+            res = 0;
+        }
+        resultado = String.valueOf(res);
+
+        tv1.setText(resultado);
+    }
+}
